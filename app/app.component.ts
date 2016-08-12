eb6a562b0eac4e7dd6e0f578b554695e75e17ace
sr <skyrousse@gmail.com>
2016-08-12 12:18:26 -0700

    update styles and add header image section to the top of the page

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,14 @@ import { Meal } from './meal.model';
   directives: [MealListComponent],
   template:`
     <div class="app-wrapper">
+      <div class="header-images">
+        <img src="/resources/images/egg-icon.png" alt="egg-icon">
+        <img src="/resources/images/milk-icon.png" alt="milk-icon">
+        <img src="/resources/images/peanut-icon.png" alt="peanut-icon">
+        <img src="/resources/images/soy-icon.png" alt="soy-icon">
+        <img src="/resources/images/wheat-icon.png" alt="wheat-icon">
+        <img src="/resources/images/fish-icon.png" alt="fish-icon">
+      </div>
       <h1>Meal Tracker</h1>
       <meal-list [mealList]="meals"></meal-list>
     </div>
