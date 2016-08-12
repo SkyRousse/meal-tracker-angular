dfc65bfcbb5837420f4002bc6cc83142d67e0741
sr <skyrousse@gmail.com>
2016-08-11 23:05:59 -0700

    add meallist component

---
+++ app/meal-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+import { MealComponent } from './meal.component';
+
+@Component({
+  selector: 'meal-list',
+  template:`
+    <meal-display *ngFor="#currentMeal of mealList" [meal]="currentMeal">
+
+    </meal-display>
+  `
+})
+export class MealListComponent {
+  public mealList: Meal[];
+  constructor(){}
+}
